cff9660d209ef0065dc835d86b7072b158d9682f
hrtarposhyan <hrtarposhyan@gmail.com>
2020-06-13 03:06:02 +0400

    Initialize DB with test data Create Controller and views

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace ContosoUniversity.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
